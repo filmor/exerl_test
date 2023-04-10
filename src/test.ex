91a7501ae1ded84d9dc9de072e22348bd8031019
Benedikt Reinartz <filmor@gmail.com>
2023-04-10 22:42:57 +0200

    Initial commit

---
+++ src/test.ex
@@ -0,0 +1,50 @@
+defmodule Bla do
+  def a() do
+    1
+  end
+end
+defmodule Bla1 do
+  def a() do
+    1
+  end
+end
+defmodule Bla2 do
+  def a() do
+    1
+  end
+end
+defmodule Bla3 do
+  def a() do
+    1
+  end
+end
+defmodule Bla4 do
+  def a() do
+    1
+  end
+end
+defmodule Bla5 do
+  def a() do
+    1
+  end
+end
+defmodule Bla6 do
+  def a() do
+    1
+  end
+end
+defmodule Bla7 do
+  def a() do
+    1
+  end
+end
+defmodule Bla8 do
+  def a() do
+    1
+  end
+end
+defmodule Bla9 do
+  def a() do
+    1
+  end
+end
---
+++ src/tzdata_test.ex
@@ -0,0 +1,5 @@
+defmodule Blubb do
+  def aliases do
+    TzData.zone_alias_list()
+  end
+end
